c11a5331ed5720770aebeb4e6c4f098f07b6ab88
desolt <desolt@protonmail.com>
2017-08-04 03:38:32 -0400

    fixed sprites not having any transparency.

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -1,12 +1,15 @@
 #version 330 core
 
 in vec2 tex_coord;
-
 out vec4 frag_color;
 
 uniform sampler2D tex;
 
 void main()
 {
-    frag_color = texture(tex, tex_coord);
+    vec4 c = texture(tex, tex_coord);
+    if (c.a < 0.5) discard;
+
+    frag_color = vec4(c.rgb, 1);
+
 }
